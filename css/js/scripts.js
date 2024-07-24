79336240a8546a444d34f14728addf2be5520364
squatforce <vukovic.ivan.fit@gmail.com>
2024-07-24 21:35:40 +0300

    Update scripts.js

--- css/js/scripts.js
+++ css/js/scripts.js
@@ -2,3 +2,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('SquatForce website loaded');
 });
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
