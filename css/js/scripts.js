3e022827ea3cd9a30aebb3143d60c50e4c35d194
squatforce <vukovic.ivan.fit@gmail.com>
2024-07-24 21:47:18 +0300

    Update scripts.js

--- css/js/scripts.js
+++ css/js/scripts.js
@@ -9,5 +9,27 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.style.display = i === index ? 'block' : 'none';
     });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(currentSlide);
+    setInterval(nextSlide, 3000); // Automatski mijenja slajdove svakih 3 sekunde
+});
 });
